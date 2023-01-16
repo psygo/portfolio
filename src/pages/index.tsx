a4fd9732da502e1a428081c90a9c372d3c0eefcc
Philippe Fanaro <philippefanaro@gmail.com>
2023-01-16 10:24:19 -0300

    first steps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,6 +9,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main className="bg-gray-200 px-10">
+        <section className="min-h-screen">
+          <nav className="flex justify-between py-8 mb-12">
+            <h1>Philippe Fanaro</h1>
+            <ul>
+              <li>
+                <a href="#">CV</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
     </>
   );
 }
